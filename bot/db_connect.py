5535a0684e6f152c5c920b9a842c18bbbda0d4ed
macbook <romes57@gmail.com>
2023-11-28 16:57:58 +0300

    test db connect kwargs

--- bot/db_connect.py
+++ bot/db_connect.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from bot.settings_bot import settings
 
-engine = create_async_engine(settings.database_url)
+engine = create_async_engine(settings.database_url, pool_recycle=100, max_overflow=30, pool_size=10, pool_timeout=1000)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
