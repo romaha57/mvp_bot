26ec1cc97a05759420f19abbc972332db808251c
macbook <romes57@gmail.com>
2024-02-08 16:29:55 +0300

    add knowledge_base

--- bot/knowledge_base/handler.py
+++ bot/knowledge_base/handler.py
@@ -1,12 +1,14 @@
+import traceback
+
 from aiogram import Bot, Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
+from loguru import logger
 
 from bot.handlers.base_handler import Handler
 from bot.knowledge_base.keyboards import KnowledgeKeyboard
 from bot.knowledge_base.service import KnowledgeService
 from bot.settings.keyboards import BaseKeyboard
-from bot.utils.answers import collect_query_for_knowledge_base
 from bot.utils.buttons import BUTTONS
 from bot.utils.messages import MESSAGES
 
@@ -25,70 +27,89 @@ class KnowledgeHandler(Handler):
         async def knowledge_base_menu(message: Message, state: FSMContext):
             """Стартовое меню базы знаний"""
 
-            root_divides = await self.db.get_root_divides()
+            try:
 
-            msg = await message.answer(
-                MESSAGES['KNOWLEDGE_MENU'],
-                reply_markup=self.kb.divides_menu(root_divides)
-            )
-            await state.update_data(base_msg=msg.message_id)
-            await state.update_data(chat_id=message.chat.id)
+                root_divides = await self.db.get_root_divides()
+
+                msg = await message.answer(
+                    MESSAGES['KNOWLEDGE_MENU'],
+                    reply_markup=self.kb.divides_menu(root_divides)
+                )
+                await state.update_data(base_msg=msg.message_id)
+                await state.update_data(chat_id=message.chat.id)
+
+            except Exception:
+                logger.warning(traceback.format_exc())
 
         @self.router.callback_query(F.data.startswith('divide'))
         async def get_nested_divide(callback: CallbackQuery, state: FSMContext):
             """Получение вложенной папки"""
 
-            data = await state.get_data()
+            try:
 
-            root_divide_id = callback.data.split('_')[-1]
+                data = await state.get_data()
 
-            divides = await self.db.get_divides_by_root(root_divide_id)
-            files = await self.db.get_files_by_divide(root_divide_id)
+                root_divide_id = callback.data.split('_')[-1]
 
-            if data.get('base_msg'):
-                await callback.bot.edit_message_reply_markup(
-                    chat_id=data.get('chat_id'),
-                    message_id=data.get('base_msg'),
-                    reply_markup=self.kb.divides_menu(divides, files)
-                )
+                divides = await self.db.get_divides_by_root(root_divide_id)
+                files = await self.db.get_files_by_divide(root_divide_id)
+
+                if data.get('base_msg'):
+                    await callback.bot.edit_message_reply_markup(
+                        chat_id=data.get('chat_id'),
+                        message_id=data.get('base_msg'),
+                        reply_markup=self.kb.divides_menu(divides, files)
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
 
         @self.router.callback_query(F.data.startswith('file'))
         async def get_knowledge_base_file(callback: CallbackQuery, state: FSMContext):
             """Получение файла базы знаний"""
 
-            file_id = callback.data.split('_')[-1]
+            try:
 
-            file = await self.db.get_file_by_id(file_id)
-            file_text = file.title
-            if file.description:
-                file_text += f'\n{file.description}'
+                file_id = callback.data.split('_')[-1]
 
-            if file.type_id == 1:
-                await callback.message.answer_document(
-                    document=file.document,
-                    caption=file_text
-                )
+                file = await self.db.get_file_by_id(file_id)
+                file_text = file.title
+                if file.description:
+                    file_text += f'\n{file.description}'
 
-            elif file.type_id == 2:
-                await callback.message.answer_video(
-                    video=file.document,
-                    caption=file_text
-                )
+                if file.type_id == 1:
+                    await callback.message.answer_document(
+                        document=file.document,
+                        caption=file_text
+                    )
+
+                elif file.type_id == 2:
+                    await callback.message.answer_video(
+                        video=file.document,
+                        caption=file_text
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
 
         @self.router.callback_query(F.data.startswith('baseback'))
         async def back_button(callback: CallbackQuery, state: FSMContext):
             """Отлов кнопки нахад в меню базы знаний"""
+            try:
 
-            data = await state.get_data()
-            parent_id = callback.data.split('_')[-1]
-            parent_id = int(parent_id) - 1
+                data = await state.get_data()
+                parent_id = callback.data.split('_')[-1]
+                parent_id = int(parent_id) - 1
 
-            divides = await self.db.get_divides_by_root(parent_id)
-            files = await self.db.get_files_by_divide(parent_id)
+                divides = await self.db.get_divides_by_root(parent_id)
+                files = await self.db.get_files_by_divide(parent_id)
 
-            if data.get('base_msg'):
-                await callback.bot.edit_message_reply_markup(
-                    chat_id=data.get('chat_id'),
-                    message_id=data.get('base_msg'),
-                    reply_markup=self.kb.divides_menu(divides, files)
-                )
+                if data.get('base_msg'):
+                    await callback.bot.edit_message_reply_markup(
+                        chat_id=data.get('chat_id'),
+                        message_id=data.get('base_msg'),
+                        reply_markup=self.kb.divides_menu(divides, files)
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
