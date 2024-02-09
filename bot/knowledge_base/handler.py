83e37698788125d0ca15f1177093ed42d2d02178
macbook <romes57@gmail.com>
2024-02-09 14:24:54 +0300

    add question fullname for certificate

--- bot/knowledge_base/handler.py
+++ bot/knowledge_base/handler.py
@@ -1,12 +1,16 @@
+import traceback
+
 from aiogram import Bot, F, Router
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from loguru import logger
 
 from bot.handlers.base_handler import Handler
 from bot.knowledge_base.keyboards import KnowledgeKeyboard
 from bot.knowledge_base.service import KnowledgeService
 from bot.settings.keyboards import BaseKeyboard
 from bot.utils.buttons import BUTTONS
+from bot.utils.messages import MESSAGES
 
 
 class KnowledgeHandler(Handler):
@@ -26,89 +30,89 @@ class KnowledgeHandler(Handler):
             await message.answer('Раздел находится в разработке',
                                  reply_markup=await self.base_kb.start_btn(promocode))
 
-        #     try:
-        #
-        #         root_divides = await self.db.get_root_divides()
-        #
-        #         msg = await message.answer(
-        #             MESSAGES['KNOWLEDGE_MENU'],
-        #             reply_markup=self.kb.divides_menu(root_divides)
-        #         )
-        #         await state.update_data(base_msg=msg.message_id)
-        #         await state.update_data(chat_id=message.chat.id)
-        #
-        #     except Exception:
-        #         logger.warning(traceback.format_exc())
-        #
-        # @self.router.callback_query(F.data.startswith('divide'))
-        # async def get_nested_divide(callback: CallbackQuery, state: FSMContext):
-        #     """Получение вложенной папки"""
-        #
-        #     try:
-        #
-        #         data = await state.get_data()
-        #
-        #         root_divide_id = callback.data.split('_')[-1]
-        #
-        #         divides = await self.db.get_divides_by_root(root_divide_id)
-        #         files = await self.db.get_files_by_divide(root_divide_id)
-        #
-        #         if data.get('base_msg'):
-        #             await callback.bot.edit_message_reply_markup(
-        #                 chat_id=data.get('chat_id'),
-        #                 message_id=data.get('base_msg'),
-        #                 reply_markup=self.kb.divides_menu(divides, files)
-        #             )
-        #
-        #     except Exception:
-        #         logger.warning(traceback.format_exc())
-        #
-        # @self.router.callback_query(F.data.startswith('file'))
-        # async def get_knowledge_base_file(callback: CallbackQuery, state: FSMContext):
-        #     """Получение файла базы знаний"""
-        #
-        #     try:
-        #
-        #         file_id = callback.data.split('_')[-1]
-        #
-        #         file = await self.db.get_file_by_id(file_id)
-        #         file_text = file.title
-        #         if file.description:
-        #             file_text += f'\n{file.description}'
-        #
-        #         if file.type_id == 1:
-        #             await callback.message.answer_document(
-        #                 document=file.document,
-        #                 caption=file_text
-        #             )
-        #
-        #         elif file.type_id == 2:
-        #             await callback.message.answer_video(
-        #                 video=file.document,
-        #                 caption=file_text
-        #             )
-        #
-        #     except Exception:
-        #         logger.warning(traceback.format_exc())
-        #
-        # @self.router.callback_query(F.data.startswith('baseback'))
-        # async def back_button(callback: CallbackQuery, state: FSMContext):
-        #     """Отлов кнопки нахад в меню базы знаний"""
-        #     try:
-        #
-        #         data = await state.get_data()
-        #         parent_id = callback.data.split('_')[-1]
-        #         parent_id = int(parent_id) - 1
-        #
-        #         divides = await self.db.get_divides_by_root(parent_id)
-        #         files = await self.db.get_files_by_divide(parent_id)
-        #
-        #         if data.get('base_msg'):
-        #             await callback.bot.edit_message_reply_markup(
-        #                 chat_id=data.get('chat_id'),
-        #                 message_id=data.get('base_msg'),
-        #                 reply_markup=self.kb.divides_menu(divides, files)
-        #             )
-        #
-        #     except Exception:
-        #         logger.warning(traceback.format_exc())
+            try:
+
+                root_divides = await self.db.get_root_divides()
+
+                msg = await message.answer(
+                    MESSAGES['KNOWLEDGE_MENU'],
+                    reply_markup=self.kb.divides_menu(root_divides)
+                )
+                await state.update_data(base_msg=msg.message_id)
+                await state.update_data(chat_id=message.chat.id)
+
+            except Exception:
+                logger.warning(traceback.format_exc())
+
+        @self.router.callback_query(F.data.startswith('divide'))
+        async def get_nested_divide(callback: CallbackQuery, state: FSMContext):
+            """Получение вложенной папки"""
+
+            try:
+
+                data = await state.get_data()
+
+                root_divide_id = callback.data.split('_')[-1]
+
+                divides = await self.db.get_divides_by_root(root_divide_id)
+                files = await self.db.get_files_by_divide(root_divide_id)
+
+                if data.get('base_msg'):
+                    await callback.bot.edit_message_reply_markup(
+                        chat_id=data.get('chat_id'),
+                        message_id=data.get('base_msg'),
+                        reply_markup=self.kb.divides_menu(divides, files)
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
+
+        @self.router.callback_query(F.data.startswith('file'))
+        async def get_knowledge_base_file(callback: CallbackQuery, state: FSMContext):
+            """Получение файла базы знаний"""
+
+            try:
+
+                file_id = callback.data.split('_')[-1]
+
+                file = await self.db.get_file_by_id(file_id)
+                file_text = file.title
+                if file.description:
+                    file_text += f'\n{file.description}'
+
+                if file.type_id == 1:
+                    await callback.message.answer_document(
+                        document=file.document,
+                        caption=file_text
+                    )
+
+                elif file.type_id == 2:
+                    await callback.message.answer_video(
+                        video=file.document,
+                        caption=file_text
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
+
+        @self.router.callback_query(F.data.startswith('baseback'))
+        async def back_button(callback: CallbackQuery, state: FSMContext):
+            """Отлов кнопки нахад в меню базы знаний"""
+            try:
+
+                data = await state.get_data()
+                parent_id = callback.data.split('_')[-1]
+                parent_id = int(parent_id) - 1
+
+                divides = await self.db.get_divides_by_root(parent_id)
+                files = await self.db.get_files_by_divide(parent_id)
+
+                if data.get('base_msg'):
+                    await callback.bot.edit_message_reply_markup(
+                        chat_id=data.get('chat_id'),
+                        message_id=data.get('base_msg'),
+                        reply_markup=self.kb.divides_menu(divides, files)
+                    )
+
+            except Exception:
+                logger.warning(traceback.format_exc())
