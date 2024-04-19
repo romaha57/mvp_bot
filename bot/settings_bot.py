0439c4ba57b6d89b210ccedce915ceefe66890e3
macbook <romes57@gmail.com>
2024-04-19 12:21:33 +0300

    version 2.0(multy course/quiz)

--- bot/settings_bot.py
+++ bot/settings_bot.py
@@ -12,8 +12,7 @@ class Settings:
         self.database_url = os.getenv('DATABASE_URL')
         self.database_url_local = os.getenv('DATABASE_URL_LOCAL')
         self.log_lvl = os.getenv('LOG_LVL')
-        self.debug = os.getenv('DEBUG')
-        print(self.debug)
+        self.debug = os.getenv('DEBUG') or 0
 
         if bool(int(self.debug)):
             self.bot_token = os.getenv('BOT_TOKEN_LOCAL')
