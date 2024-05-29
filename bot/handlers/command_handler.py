d8050c2b68410f650e341d7fbb9d26ecbb3302be
macbook <romes57@gmail.com>
2024-05-29 16:55:55 +0300

    fix font/go to menu from answers

--- bot/handlers/command_handler.py
+++ bot/handlers/command_handler.py
@@ -69,106 +69,97 @@ class CommandHandler(Handler):
                     if promocode and promocode.actual:
                         courses_by_promo = await self.db.get_courses_by_promo(promocode.id)
 
-                        if promocode.end_at <= datetime.datetime.now():
-                            courses_and_quizes = await self.db.get_promocode_courses_and_quizes(promocode.id)
+                        # увеличиваем счетчик активированных пользователей на этом промокоде
+                        await self.db.increment_count_promocode(
+                            promocode
+                        )
+
+                        # сохраняем пользователя в БД
+                        await self.user_db.get_or_create_user(
+                            username=message.from_user.username,
+                            tg_id=message.chat.id,
+                            bot_id=promocode.bot_id,
+                            first_name=message.from_user.first_name,
+                            last_name=message.from_user.last_name
+                        )
+
+                        user = await self.db.get_user_by_tg_id(message.chat.id)
+
+                        if courses_by_promo:
+                            courses_titles = '\n'.join([f" - {course.get('title')}" for course in courses_by_promo])
                             await message.answer(
-                                MESSAGES['YOUR_PROMOCODE_IS_EXPIRED'],
-                                reply_markup=await self.kb.start_btn(courses_and_quizes)
+                                MESSAGES['START_PROMOCODE'].format(
+                                    courses_titles
+                                )
                             )
+                        logger.debug(f"Пользователь {message.from_user.id} активировал промокод {promocode.code}")
+
+                        if promocode.is_test and user.promocode_id and not promocode_from_db.is_test:
+                            courses_and_quizes = await self.db.get_promocode_courses_and_quizes(user.promocode_id)
+                            await message.answer(
+                                MESSAGES['MENU'],
+                                reply_markup=await self.kb.start_btn(courses_and_quizes))
 
                         else:
-
-                            # увеличиваем счетчик активированных пользователей на этом промокоде
-                            await self.db.increment_count_promocode(
-                                promocode
-                            )
-
-                            # сохраняем пользователя в БД
-                            await self.user_db.get_or_create_user(
-                                username=message.from_user.username,
+                            await self.db.add_promocode_to_user(
                                 tg_id=message.chat.id,
-                                bot_id=promocode.bot_id,
-                                first_name=message.from_user.first_name,
-                                last_name=message.from_user.last_name
+                                promocode_id=promocode.id
                             )
 
-                            user = await self.db.get_user_by_tg_id(message.chat.id)
-
-                            if courses_by_promo:
-                                courses_titles = '\n'.join([f" - {course.get('title')}" for course in courses_by_promo])
-                                await message.answer(
-                                    MESSAGES['START_PROMOCODE'].format(
-                                        courses_titles
-                                    )
-                                )
-                            logger.debug(f"Пользователь {message.from_user.id} активировал промокод {promocode.code}")
-
-                            if promocode.is_test and user.promocode_id and not promocode_from_db.is_test:
-                                courses_and_quizes = await self.db.get_promocode_courses_and_quizes(user.promocode_id)
-                                await message.answer(
-                                    MESSAGES['MENU'],
-                                    reply_markup=await self.kb.start_btn(courses_and_quizes))
-
-                            else:
-                                await self.db.add_promocode_to_user(
-                                    tg_id=message.chat.id,
-                                    promocode_id=promocode.id
+                            user_account = await self.db.get_account_by_tg_id(message.chat.id)
+                            if promocode.account_id and user_account.id != promocode.account_id:
+                                await self.db.add_promocode_partners(
+                                    promocode=promocode,
+                                    user=user_account
                                 )
 
-                                user_account = await self.db.get_account_by_tg_id(message.chat.id)
-                                if promocode.account_id and user_account.id != promocode.account_id:
-                                    await self.db.add_promocode_partners(
-                                        promocode=promocode,
-                                        user=user_account
-                                    )
-
-                                if user.accept_politics:
-                                    anketa_questions = await check_user_anket(
+                            if user.accept_politics:
+                                anketa_questions = await check_user_anket(
+                                    message=message,
+                                    user=user
+                                )
+                                if anketa_questions:
+                                    await get_user_answers_for_anketa(
                                         message=message,
-                                        user=user
+                                        state=state,
+                                        questions=anketa_questions
                                     )
-                                    if anketa_questions:
-                                        await get_user_answers_for_anketa(
-                                            message=message,
-                                            state=state,
-                                            questions=anketa_questions
+
+                                else:
+
+                                    if promocode.is_test:
+                                        now = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
+                                        user = await self.db.get_user_by_tg_id(message.chat.id)
+                                        await self.db.set_start_and_end_time_test_promo(
+                                            now=now,
+                                            promocode=promocode,
+                                            user=user
                                         )
+                                        await message.answer(
+                                            MESSAGES['GO_TO_TEST_PROMOCODE'],
+                                            reply_markup=await self.test_promo_kb.test_promo_menu())
+
+                                    elif promocode.type_id == 3:
+                                        await message.answer(
+                                            MESSAGES['START_PROMOCODE_OWNER'],
+                                            reply_markup=await self.kb.start_btn(promocode))
 
                                     else:
-
-                                        if promocode.is_test:
-                                            now = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
-                                            user = await self.db.get_user_by_tg_id(message.chat.id)
-                                            await self.db.set_start_and_end_time_test_promo(
-                                                now=now,
-                                                promocode=promocode,
-                                                user=user
-                                            )
-                                            await message.answer(
-                                                MESSAGES['GO_TO_TEST_PROMOCODE'],
-                                                reply_markup=await self.test_promo_kb.test_promo_menu())
-
-                                        elif promocode.type_id == 3:
-                                            await message.answer(
-                                                MESSAGES['START_PROMOCODE_OWNER'],
-                                                reply_markup=await self.kb.start_btn(promocode))
-
-                                        else:
-                                            courses_and_quizes = await self.db.get_promocode_courses_and_quizes(promocode.id)
-                                            await self.user_db.add_promocode_to_user(
-                                                tg_id=message.from_user.id,
-                                                promocode_id=promocode.id
-                                            )
-                                            await message.answer(
-                                                MESSAGES['MENU'],
-                                                reply_markup=await self.kb.start_btn(courses_and_quizes))
-                                else:
-                                    msg = await message.answer(
-                                        MESSAGES['ACCEPT_POLITICS'],
-                                        reply_markup=await self.kb.politics_btn()
-                                    )
-                                    await state.set_state(Politics.accept)
-                                    await state.update_data(msg=msg.message_id)
+                                        courses_and_quizes = await self.db.get_promocode_courses_and_quizes(promocode.id)
+                                        await self.user_db.add_promocode_to_user(
+                                            tg_id=message.from_user.id,
+                                            promocode_id=promocode.id
+                                        )
+                                        await message.answer(
+                                            MESSAGES['MENU'],
+                                            reply_markup=await self.kb.start_btn(courses_and_quizes))
+                            else:
+                                msg = await message.answer(
+                                    MESSAGES['ACCEPT_POLITICS'],
+                                    reply_markup=await self.kb.politics_btn()
+                                )
+                                await state.set_state(Politics.accept)
+                                await state.update_data(msg=msg.message_id)
                     else:
                         msg = await self.db.get_msg_by_key('bad_promocode')
                         kb = await self.kb.help_btn()
